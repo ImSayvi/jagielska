bbb0db09f2f254e3f5d617f99f8a7d1c5d329cce
Say.vi <say.julia.vi@gmail.com>
2025-07-13 21:00:36 +0200

    ffs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import Navbar from "./components/Navbar/Navbar"
-import Hero from "./components/Hero/hero"
+import Hero from "./components/Hero/Hero"
 import Services from "./components/Services/Services";
 import Services2 from "./components/Services/Services2";
 import Contact from "./components/Contact/contact";
